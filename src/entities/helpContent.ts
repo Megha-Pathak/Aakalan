8cba39249753976958c03b6918196d539f237d9d
Megha <meghapathak2013@gmail.com>
2020-12-03 21:05:03 +0530

    Initial Upload

---
+++ src/entities/helpContent.ts
@@ -0,0 +1,19 @@
+export const helpContent = [
+  {
+    title: `What's Aakalan?`,
+    content: [
+      `Add some content here`,
+    ],
+  },
+  {
+    title: 'What all I can do with Aakalan?',
+    content: [
+      `Add some content here`,
+    ],
+  },
+  {
+    title: 'When to use Aakalan?',
+    content: [`Add some content here`],
+  },
+
+];
